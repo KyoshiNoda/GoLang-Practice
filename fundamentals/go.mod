092b4fc827976cf64170abf366e55c59bc70be5c
KyoshiNoda <kyoshisew@gmail.com>
2023-03-01 17:28:27 -0500

    added courses withs students

--- fundamentals/go.mod
+++ fundamentals/go.mod
@@ -1,3 +1,3 @@
-module test
+module fundamentals
 
 go 1.19
--- fundamentals/main.go
+++ fundamentals/main.go
@@ -1,11 +1,60 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
+type Student struct {
+	name   string
+	age    int
+	gender string
+}
+
+func addStudent(name string, age int, gender string) Student {
+	var currentStudent Student
+	currentStudent.name = name
+	currentStudent.age = age
+	currentStudent.gender = gender
+	return currentStudent
+}
+func printStudent(current Student) {
+	fmt.Println("Name:" + current.name)
+	fmt.Println("Age: " + strconv.Itoa(current.age))
+	fmt.Println("Gender: " + current.gender)
+}
 func main() {
-	var name = "kyoshi"
-	// fmt.Scan(&name)
 
-	// fmt.Printf("user:%v", name)
-	fmt.Println(&name)
+	input := true
+	var class []Student
+	var name string
+	var age int
+	var gender string
+	var response string
+
+	fmt.Println("Welcome to University!")
+	for {
+		var tempStudent Student
+		fmt.Println("Would you like to add a student?")
+		fmt.Scanln(&response)
+		if response == "No" || response == "no" {
+			input = false
+		}
+		if !input {
+			break
+		}
+		fmt.Println("What is your name?")
+		fmt.Scanln(&name)
+		fmt.Println("What is your age?")
+		fmt.Scanln(&age)
+		fmt.Println("What is your gender?")
+		fmt.Scanln(&gender)
+		tempStudent = addStudent(name, age, gender)
+		class = append(class, tempStudent)
+	}
+	for i := 0; i < len(class); i++ {
+		printStudent(class[i])
+		fmt.Println()
+	}
+
 }
